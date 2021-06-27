bae5953890c74945b9fe11419d87806015c47582
Jshkid123 <49582920+Jshkid123@users.noreply.github.com>
2021-06-27 16:23:33 +0400

    Fixed an issue where an extra space would be added to comment. Added comments

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-import re
 import os
 import pandas
 import praw
 from prawcore.exceptions import ResponseException
+from keep_alive import keep_alive
 
-reddit=praw.Reddit(
-  user_agent= "<UWBot1.0>"
+reddit=praw.Reddit(                                             #Auhtorization information deleted for security
+  user_agent= "<UWBot1.0>" 
 )
-try:
-    print("Authenticated as {}".format(reddit.user.me()))
+
+
+try:                                                            #Checks authorization details and connects to the Reddit API
+    print("Authenticated as {}".format(reddit.user.me())) 
 except ResponseException:
     print("Something went wrong during authentication")
 
-subreddit= reddit.subreddit("enghacksbot2222")
+subreddit= reddit.subreddit("enghacksinfo")
 
 
+df= pandas.read_csv('Merged.csv')                               #Read's the UWaterloo CSV file for course information
 
-df= pandas.read_csv('Merged.csv')
 
-courses=[]
+courses=[]                                                      #Creates arrays to store different values from the CSV file
 courseCode=[]
 courseDesc={}
 commentID=[]
 
+                                                                #Adding infomation into the different arrays from CSV file
 for course in df["subjectCode"]:
    courses.append(course)
 for course in df["catalogNumber"]:
@@ -42,14 +45,23 @@ for course in df["description"]:
    else:
      break
 
-for comment in subreddit.stream.comments(skip_existing=True):
+for comment in subreddit.stream.comments(skip_existing=True):  #Loops through a reddit comment looking for information
 
-  result = re.sub("[A-Za-z]+", lambda ele: " " + ele[0] + " ", comment.body)
+  result = ""                                                  #Normalizes the comment to a recognizable format
+  alpha = ""
+  num = ""
+
+  for letter in comment.body:
+    if letter.isalpha():
+      alpha=alpha+letter
+    elif letter.isnumeric():
+      num = num + letter
+  
+  result = alpha + " " + num
   result = result.upper()
-  result=result.strip()
 
   TmpCourse = ""
-  print(commentID)
+  print(commentID)                                            #prints different results for testing *can be removed*
   print(result)
   print(len(result))
   IsThere = False
@@ -66,3 +78,4 @@ for comment in subreddit.stream.comments(skip_existing=True):
     elif (IsThere == False):
       comment.reply("Course not found, for information on the courses offered at UWaterloo, use this: ecampusontario.ca/?itemTypes=1&itemTypes=2&itemTypes=3&sourceWebsiteTypes=2&institutions=426&sortCol")
       commentID.append(comment.id)
+  keep_alive()
